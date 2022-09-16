cac9c7dc9d7e30172bd98037d6b0249855740d50
SysBioUAB <78474998+SysBioUAB@users.noreply.github.com>
2022-09-16 20:39:47 +0200

    Update select_enriched.py

--- select_enriched.py
+++ select_enriched.py
@@ -18,29 +18,28 @@ awk = subprocess.Popen('LC_NUMERIC=C awk \'{if ($3 + 0 > 0.5 && $2 + 0 < 0.05) {
 domains = awk.communicate()[0].decode().split('\n')[1:]
 
 # Take only the enriched motif/domains from the host/pathogen (values above 1)
-motif_enrichment = subprocess.Popen('LC_NUMERIC=C awk \'{if ($2 + 0 > 1) {print $1}}\' outputs/host_domain_enrichment_01.txt',stdout=subprocess.PIPE,shell=True)
+motif_enrichment = subprocess.Popen('LC_NUMERIC=C awk \'{if ($2 + 0 > 1) {print $1}}\' outputs/host_domain_enrichment.txt',stdout=subprocess.PIPE,shell=True)
 motif_enrichment_only_enriched = motif_enrichment.communicate()[0].decode().split('\n')[1:]
 
 domain_enrichment = subprocess.Popen('LC_NUMERIC=C awk \'{if ($2 + 0 > 1) {print $1}}\' outputs/pathogen_domain_enrichment.txt',stdout=subprocess.PIPE,shell=True)
 domain_enrichment_only_enriched = domain_enrichment.communicate()[0].decode().split('\n')[1:]
 
 
-with open('outputs/domain-domain_observed_freq_prueba.txt','rt') as f_obs:
+with open('outputs/domain-domain_observed_freq.txt','rt') as f_obs:
 	for line in f_obs:
 		record_obs = line.split()
 		if int(record_obs[0]) >= cutoff:
 			interactions_with_obs_freq[record_obs[-2]] = record_obs[-1].replace(',','.')
 
 
-with open('outputs/domain-domain_random_freq_prueba.txt', 'rt') as f_rand:
+with open('outputs/domain-domain_random_freq.txt', 'rt') as f_rand:
 	for line in f_rand:
 		record_rand = line.split(',')
 		all_rand_freq = record_rand[1:]
 		rand_freq = statistics.fmean([float(i) for i in all_rand_freq])
 		interactions_with_rand_freq[record_rand[0]] = rand_freq
 
-# for each domain, if the domain is in the observed domains and it appears 10 or more times, take the observed frequency, then search this same domain in the random domains
-# and compute the mean of all the 1000 frequencies. Finally, compute the fobs/frand for the selected domains.
+# Compute the fobs/frand for the selected domains.
 for i in domains:
 	try:
 		if i in interactions_with_obs_freq:
@@ -79,7 +78,7 @@ for i in domains:
 					rand_freq = float(interactions_with_rand_freq[i])
 
 
-					with open('outputs/domain_domain_enrichment_nuevo_all.txt','at') as f:
+					with open('outputs/domain_domain_enrichment.txt','at') as f:
 						f.write(i+'\t'+str(obs_freq/rand_freq)+'\n')
 
 	except:
