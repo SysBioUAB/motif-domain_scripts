e4aa9a7df2ee9e4137aa279c991272a28e899425
SysBioUAB <78474998+SysBioUAB@users.noreply.github.com>
2022-09-16 19:16:57 +0200

    Add files via upload

---
+++ interpro_to_pfam.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Extract the PFAM records from the InterProScan output. 
+if [[ $# != 2 ]] ; then    
+    echo 'ERROR: Wrong arguments given. Usage: bash' "$0" '[HOST_PROTEOME_IN_FASTA PATH] [PFAM_FOLDER PATH]'
+    exit
+fi
+# Change extension if needed ( .fasta or .fasta.tsv)
+HOST=$(find $1 -name '*.fasta')
+
+for FILE in $HOST
+do
+	echo $(basename "$FILE")
+	if [[ $(grep "Pfam" $FILE | wc -l) -gt 0 ]]
+	then
+	  grep "Pfam" $FILE | cut -f 7,8,12,13 > $2$(basename "$FILE")
+	fi
+done
+
+find $2 -size 0 -delete
